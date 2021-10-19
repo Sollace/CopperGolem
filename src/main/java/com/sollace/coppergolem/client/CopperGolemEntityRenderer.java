3cfbf84d2aa3296680b0804292f8dc04b04ff18e
Sollace <sollacea@gmail.com>
2021-10-19 22:28:12 +0200

    Added textures for each level of oxidation

--- src/main/java/com/sollace/coppergolem/client/CopperGolemEntityRenderer.java
+++ src/main/java/com/sollace/coppergolem/client/CopperGolemEntityRenderer.java
@@ -1,13 +1,17 @@
 package com.sollace.coppergolem.client;
 
+import net.minecraft.block.Oxidizable.OxidizationLevel;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
 import com.sollace.coppergolem.entity.CopperGolemEntity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopperGolemEntityRenderer extends MobEntityRenderer<CopperGolemEntity, CopperGolemEntityModel> {
-    private static final Identifier TEXTURE = new Identifier("copper_golem", "textures/entity/copper_golem/copper_golem.png");
+    private final Map<OxidizationLevel, Identifier> textures = new HashMap<>();
 
     public CopperGolemEntityRenderer(Context ctx) {
         super(ctx, new CopperGolemEntityModel(CopperGolemEntityModel.getTexturedModelData().createModel()), 0.3F);
@@ -15,6 +19,6 @@ public class CopperGolemEntityRenderer extends MobEntityRenderer<CopperGolemEnti
 
     @Override
     public Identifier getTexture(CopperGolemEntity entity) {
-        return TEXTURE;
+        return textures.computeIfAbsent(entity.getDegradationLevel(), l -> new Identifier("copper_golem", "textures/entity/copper_golem/copper_golem_" + l.name().toLowerCase() + ".png"));
     }
 }
