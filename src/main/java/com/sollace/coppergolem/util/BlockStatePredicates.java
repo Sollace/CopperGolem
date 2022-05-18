295a8f3c40d1e7f7ab2d8c97ae11d2823f356b52
David Wildasin <dragonmaus@posteo.net>
2022-05-18 14:09:13 -0400

    Update for 1.18.2

--- src/main/java/com/sollace/coppergolem/util/BlockStatePredicates.java
+++ src/main/java/com/sollace/coppergolem/util/BlockStatePredicates.java
@@ -2,13 +2,13 @@ package com.sollace.coppergolem.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 
 import java.util.function.Predicate;
 
 public interface BlockStatePredicates {
 
-    public static Predicate<BlockState> forTag(Tag<Block> tag) {
+    public static Predicate<BlockState> forTag(TagKey<Block> tag) {
         return state -> state.isIn(tag);
     }
 }
