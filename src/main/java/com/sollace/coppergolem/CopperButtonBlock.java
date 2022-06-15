28e644c5fc5c33db1d771c10d9c91abf6e83b8ce
Sollace <sollacea@gmail.com>
2022-06-15 17:27:59 +0200

    Change button pitch based on how rusted it is

--- src/main/java/com/sollace/coppergolem/CopperButtonBlock.java
+++ src/main/java/com/sollace/coppergolem/CopperButtonBlock.java
@@ -3,10 +3,15 @@ package com.sollace.coppergolem;
 import net.minecraft.block.AbstractButtonBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Oxidizable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+import org.jetbrains.annotations.Nullable;
 
 import com.sollace.coppergolem.entity.CopperGolemEntity;
 
@@ -20,6 +25,11 @@ public class CopperButtonBlock extends AbstractButtonBlock implements CustomDura
 
     protected SoundEvent getClickSound(boolean powered) {
         return powered ? SoundEvents.BLOCK_COPPER_PLACE : SoundEvents.BLOCK_COPPER_HIT;
+    }
+
+    @Override
+    protected void playClickSound(@Nullable PlayerEntity player, WorldAccess world, BlockPos pos, boolean powered) {
+        world.playSound(powered ? player : null, pos, getClickSound(powered), SoundCategory.BLOCKS, 0.3F, (powered ? 0.6F : 0.5F) / (oxidizationLevel.ordinal() + 1));
     }
 
     @Override
